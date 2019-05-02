4f018655fa71fdd91608ea3e8dcbe0455656ce50
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-05-02 18:49:08 +0300

    test: update

--- hello.js
+++ hello.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-  res.send('Hello, Heroku!')
+  res.send(hello('Heroku'));
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, function() {});
+
+const hello = someone => 'Hello, ' + someone;
+
+module.exports = hello;
