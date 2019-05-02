3cc2a059402b1ecf31f081724a45925efd0eb552
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-05-02 18:06:41 +0300

    fead: add express code

--- hello.js
+++ hello.js
@@ -1,7 +1,20 @@
-function hello(someone) {
-  return "Hello, " + someone + "!!!";
-}
+// function hello(someone) {
+//   return "Hello, " + someone + "!!!";
+// }
 
-console.log(hello("Heroku"));
+// console.log(hello("Heroku"));
 
-module.exports = hello;
+// module.exports = hello;
+
+
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send('Hello, Heroku!')
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {});
+
+//console.log("Server running at http://localhost:%d", port);
