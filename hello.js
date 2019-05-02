d2fc3f4d3b04810cf7e2570a1c5a3eb29dde36c1
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-05-02 18:14:57 +0300

    feat: update for heroku testing

--- hello.js
+++ hello.js
@@ -1,12 +1,3 @@
-// function hello(someone) {
-//   return "Hello, " + someone + "!!!";
-// }
-
-// console.log(hello("Heroku"));
-
-// module.exports = hello;
-
-
 const express = require("express");
 const app = express();
 
@@ -14,7 +5,5 @@ app.get("/", function(req, res) {
   res.send('Hello, Heroku!')
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, function() {});
-
-//console.log("Server running at http://localhost:%d", port);
