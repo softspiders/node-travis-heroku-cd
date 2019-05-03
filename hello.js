5e37e870d0deaac9eeb95d9c2fc5423eb2932b45
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-05-03 18:31:35 +0300

    feat: travis-heroku ci/cd testing

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-  res.send(hello('Heroku!!!'));
+  res.send(hello('Heroku!'));
 });
 
 const port = process.env.PORT || 3000;
